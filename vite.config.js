86486f8f0aa0dc9826fdd668d39e92fa20713c1c
Sajithjay99 <sajithjay99@gmail.com>
2026-04-25 21:27:43 +0530

    Fix base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/RP-Website-Group_25-26/-259/',
+  base: '/RP-Website-Group_25-26J-259/',
 })
